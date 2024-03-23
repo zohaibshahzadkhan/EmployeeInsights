d55abac29034a555108de3c92bae70f5fe0684d5
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-03-23 14:17:03 +0100

    Add Pandas library  and create employees DataFrame with main function

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,5 +13,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('employee_data')
 
-employees = SHEET.worksheet('employees')
-employees_data = employees.get_all_values()
+def main():
+    """
+    Run all program functions
+    """
+    employees = SHEET.worksheet('employees')
+    employees_data = employees.get_all_values()
+
+    try:
+        # Convert employees_data to DataFrame
+        employee_data_frame = pd.DataFrame(employees_data[1:], columns=employees_data[0])
+        print(employee_data_frame)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+print("Welcome to Employee Insight Survey Analyzer")
+main()
